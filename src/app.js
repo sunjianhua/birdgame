faee16a3c9696eb793e06e50888bb1ea730fabda
sunjianhua <sunjianhua@aliyun.com>
2016-08-20 19:06:54 +0800

    -

--- src/app.js
+++ src/app.js
@@ -13,27 +13,8 @@ var HelloWorldLayer = cc.Layer.extend({
         // ask the window size
         var size = cc.winSize;
 
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
-        // position the label on the center of the screen
-        helloLabel.x = size.width / 2;
-        helloLabel.y = size.height / 2 + 200;
-        // add the label as a child to this layer
-        this.addChild(helloLabel, 5);
-
-        // add "HelloWorld" splash screen"
-        this.sprite = new cc.Sprite(res.HelloWorld_png);
-        this.sprite.attr({
-            x: size.width / 2,
-            y: size.height / 2
-        });
-        this.addChild(this.sprite, 0);
-
         this.terrain = Terrain.createWithSpace();
-        this.addChild(this.sprite, 0);
+        this.addChild(this.terrain, 0);
 
         return true;
     }
